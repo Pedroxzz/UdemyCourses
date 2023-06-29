1250c2996689ad758b1365029f610be8fe9e1a21
Joao <joaopedroicm2117@gmail.com>
2023-06-29 16:38:59 -0300

    terminar

--- 100DaysOfCodePython/projects/HigherOrLower.py
+++ 100DaysOfCodePython/projects/HigherOrLower.py
@@ -1,3 +1,4 @@
+import random
 logo = """
 
  _   _ _       _                    
@@ -20,5 +21,35 @@ logo = """
       \_____/\___/ \_/\_/ \___|_|   
                                                                         
 """
+influencers = {
+    "Cristiano Ronaldo":439000000,
+    "Lionel Messi":449000000,
+    "Selena Gomez":406000000,
+    "Kylie Jenner":384000000,
+    "Dwayne Johnson":372000000,
+    "Ariana Grande":363000000,
+    "Kim Kardashian":350000000,
+    "Beyoncé":303000000,
+    "Khloé Kardashian":301000000,
+    "Justin Bieber":284000000,
+}
 
-print
+def set_opponent():
+    random_influencer = random.choice(list(influencers.keys()))
+    return random_influencer
+
+def check_answer(a,b,aws):
+    pass
+    
+
+def game():
+    print(logo)
+    a = print(set_opponent())
+    print('OR')
+    b = print(set_opponent())
+    aws = input('Who has more followers? Type "A" or "B"')
+    check_answer(a,b,aws)
+
+game()
+
+
